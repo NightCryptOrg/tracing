a89af8356d1e25ed5a937f4cd43c0e7680c48a97
very-amused <very-amused@pm.me>
2024-05-29 10:57:52 -0400

    Test child spans

--- test/format_test.go
+++ test/format_test.go
@@ -24,4 +24,17 @@ func TestFormat(t *testing.T) {
 		span := tracing.SpanContext(context.Background(), t.Name())
 		testFormat(t, span, logger)
 	})
+
+	t.Run("Multiple Spans", func(t *testing.T) {
+		const depth = 5
+
+		span := tracing.SpanContext(context.Background(), t.Name())
+		for i := 0; i < depth; i++ {
+			span = tracing.SpanContext(span, fmt.Sprintf("Inner Span %d", i))
+		}
+		testFormat(t, span, colorLogger)
+		t.Run("No Color", func(t *testing.T) {
+			testFormat(t, span, logger)
+		})
+	})
 }
